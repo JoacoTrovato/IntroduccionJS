c6c69e306245b03cba245c67ee1ca526f03ead5c
JoacoTrovato <joacotrovato@gmail.com>
2022-04-29 10:42:55 -0300

    08.JS 09.JS

---
+++ js/08.js
@@ -0,0 +1,8 @@
+const nombre = "Joaquín";
+const email = "correo@correo.com";
+
+// Concatenación 
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+// Template Strings - Strings Literals 
+console.log(`Nomre Cliente: %{nombre} Email: {email}`);
