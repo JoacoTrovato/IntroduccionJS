3ffbe6b6a14aa76f7ac3297ac6243354ff73d2b3
JoacoTrovato <joacotrovato@gmail.com>
2022-04-29 09:57:33 -0300

    02.JS 03.JS

---
+++ js/02.js
@@ -0,0 +1,24 @@
+// Variables con let
+
+let producto = "Audífonos Gamer"; // Iniciar variables y asignarle valor
+
+let disponible; // Iniciamos la variable pero sin valor.
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+let producto1 = "Computadora",
+    disponible1 = true,
+    categoria = "Computadoras";
+
+// var 1disponible;
+let disponible_;
+
+//Estilos para las variables
+let nombre_producto = "Monitor HD"; // Underscore
+let nombreProducto = "Monitor HD"; // Camel Case
+let NombrePorducto = "Monitor HD"; // Pascal Case
+let nombreproducto = "Monitor HD"; //
+
+console.log(producto)
---
+++ js/03.js
@@ -0,0 +1,24 @@
+// Variables
+
+const producto = "Audífonos Gamer"; // Iniciar variables y asignarle valor
+
+const disponible = true; // Iniciamos la variable si o si con valor.
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true; // Para poder reasignar valor la variable disponible debe ser let o var, const siempre conserva su valor constante
+
+const producto1 = "Computadora",
+    disponible1 = true,
+    categoria = "Computadoras";
+
+// var 1disponible;
+const disponible_ = true;
+
+//Estilos para las variables
+const nombre_producto = "Monitor HD"; // Underscore
+const nombreProducto = "Monitor HD"; // Camel Case
+const NombrePorducto = "Monitor HD"; // Pascal Case
+const nombreproducto = "Monitor HD"; //
+
+console.log(producto)
