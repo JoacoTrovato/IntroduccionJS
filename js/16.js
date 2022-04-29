7fa8dca248c165151c6ac96d5dcb4fe9b35155c2
JoacoTrovato <joacotrovato@gmail.com>
2022-04-29 14:09:48 -0300

    16.JS 17.JS

---
+++ js/16.js
@@ -0,0 +1,21 @@
+// Declaración de Función
+
+function sumar() {
+    console.log(10 + 10)
+};
+
+sumar();
+
+// Expreción de la Función
+
+const sumar2 = function() {
+    console.log(3 + 3)
+};
+
+sumar2();
+
+// IIFE
+
+(function() {
+    console.log("Esto es una función")
+})();
