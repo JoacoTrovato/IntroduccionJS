300adcdd27fd3b1ae9d1c932e5615efbf2570356
JoacoTrovato <joacotrovato@gmail.com>
2022-04-29 10:33:29 -0300

    06.JS 07.JS

---
+++ js/06.js
@@ -0,0 +1,16 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.5);
+resultado = Math.ceil(2.1); // Ceil siempre redondea hacia arriba
+resultado = Math.floor(2.9); // Floor siempre redondea hacia abajo
+resultado = Math.sqrt(144);
+resultado = Math.abs(2.9);
+resultado = Math.mix(3, 5, 1, 8, 2, 10); // Retorna el número menor de la variable
+resultado = Math.max(3, 5, 1, 8, 2, 10); // Retorna el número mayor de la variable
+resultado = Math.random();
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
---
+++ js/07.js
@@ -0,0 +1,26 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (600 + 600) * 1.05; // Para sacar porcentaje
+
+// console.log(resultado);
+
+// Incrementos 
+
+let puntaje = 10;
+
+// Incremento en 1
+puntaje++; // 10
+++puntaje; // 12
+
+// Decremento en 1
+puntaje--;
+--puntaje;
+
+// Incremento en valor deseado
+puntaje += 10; // Incrementa el puntaje en el valor que asignemos después del +=
+
+console.log(puntaje);
