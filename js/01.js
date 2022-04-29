c1475c40741afb559e5488535e7471810e1ee59b
JoacoTrovato <joacotrovato@gmail.com>
2022-04-29 09:51:23 -0300

    01.JS

---
+++ js/01.js
@@ -0,0 +1,24 @@
+// Variables
+
+var producto = "Audífonos Gamer"; // Iniciar variables y asignarle valor
+
+var disponible; // Iniciamos la variable pero sin valor.
+
+producto = true; // Reasignando el valor de la variable
+
+disponible = true;
+
+var producto = "Computadora",
+    disponible1 = true,
+    categoria = "Computadoras";
+
+// var 1disponible;
+var disponible_;
+
+//Estilos para las variables
+var nombre_producto = "Monitor HD"; // Underscore
+var nombreProducto = "Monitor HD"; // Camel Case
+var NombrePorducto = "Monitor HD"; // Pascal Case
+var nombreproducto = "Monitor HD"; //
+
+console.log(producto)
